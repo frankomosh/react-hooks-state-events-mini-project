58d7f8e2601a7144a7a980c10b3fe866f3274327
Frank-197 <frankomosh197@gmail.com>
2022-07-25 09:57:42 +0300

    Done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
+function TaskList({ tasks, onDelete }) {
+  const taskList = tasks.map((task, index) => <Task key={task.text}
+  text={task.text} category={task.category} onDeleteTask={onDelete} id={index}/>)
+
+ return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
